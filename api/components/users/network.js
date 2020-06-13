3ba0cefb74d724e16f661852c59759f395671eb4
emanuelosva <emanuelosva@gmail.com>
2020-06-12 21:21:32 -0500

    improve catch errors

--- api/components/users/network.js
+++ api/components/users/network.js
@@ -22,44 +22,36 @@ router.put('/', authenticate('update'), upsertUser);
 router.delete('/:id', removeUser);
 
 // CALLBACKS
-function listUsers(req, res) {
+function listUsers(req, res, next) {
   controller.list()
     .then(list => {
       response.succes(req, res, list, 200);
     })
-    .catch(err => {
-      response.error(req, res, '', 500, err);
-    });
+    .catch(next);
 };
 
-function getUser(req, res) {
+function getUser(req, res, next) {
   controller.get(req.params.id)
     .then(user => {
       response.succes(req, res, user, 200);
     })
-    .catch(err => {
-      response.error(req, res, '', 500, err);
-    });
+    .catch(next);
 };
 
-function upsertUser(req, res) {
+function upsertUser(req, res, next) {
   controller.upsert(req.body)
     .then(user => {
       response.succes(req, res, user, 201);
     })
-    .catch(err => {
-      response.error(req, res, '', 400, err);
-    });
+    .catch(next);
 };
 
-function removeUser(req, res) {
+function removeUser(req, res, next) {
   controller.remove(req.params.id)
     .then(deleted => {
       response.succes(req, res, deleted, 200);
     })
-    .catch(err => {
-      response.error(req, res, '', 500, err);
-    });
+    .catch(next);
 };
 
 module.exports = router;
