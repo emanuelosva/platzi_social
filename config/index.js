167ab1a15648918d25eaaaee24dd92750a376e79
emanuelosva <emanuelosva@gmail.com>
2020-06-15 12:27:59 -0500

    Env variables updated

--- config/index.js
+++ config/index.js
@@ -26,11 +26,11 @@ module.exports = {
 
   // MySQL CONFGIG (Local)
   mysql: {
-    host: process.env.MYSQL_HOST || 'remotemysql.com',
-    user: process.env.MYSQL_USER || 'bXT7aZzFdg',
-    port: process.env.MYSQL_PORT || '3306',
-    password: process.env.MYSQL_PASSWORD || 'Ha5lksthXG',
-    database: process.env.MYSQL_DATABASE || 'bXT7aZzFdg',
+    host: process.env.MYSQL_HOST || '',
+    user: process.env.MYSQL_USER || '',
+    port: process.env.MYSQL_PORT || '',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || '',
   },
 
   // MySQL MicroServcie CONFGIG
